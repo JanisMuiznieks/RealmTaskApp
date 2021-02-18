521ad072b5409f665258dac2cdf4caefa20a5ce7
JanisMuiznieks <janis.muiznieks8@gmail.com>
2021-02-18 11:25:12 +0200

    Tasks list app with Realm

---
+++ RealmTaskApp/Model/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  RealmTaskApp
+//
+//  Created by janis.muiznieks on 17/02/2021.
+//
+
+import Foundation
+import RealmSwift
+
+
+class Task: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var note = ""
+    @objc dynamic var date = Date()
+    @objc dynamic var isComplete = false 
+}
